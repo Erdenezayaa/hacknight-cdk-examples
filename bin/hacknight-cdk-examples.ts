0b6a26ec2ecab581ae5027717efad6b739326f82
Erdenezaya <d.erdenezaya@gmail.com>
2022-04-20 19:48:01 +0800

    Initial commit

--- bin/hacknight-cdk-examples.ts
+++ bin/hacknight-cdk-examples.ts
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { HacknightCdkExamplesStack } from '../lib/hacknight-cdk-examples-stack';
+import { ServerlessStack } from '../lib/serverless-stack';
 
 const app = new cdk.App();
-new HacknightCdkExamplesStack(app, 'HacknightCdkExamplesStack', {
+new ServerlessStack(app, 'HacknightCdkExamplesStack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
--- lib/serverless-stack.ts
+++ lib/serverless-stack.ts
@@ -2,7 +2,7 @@ import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
-export class HacknightCdkExamplesStack extends Stack {
+export class ServerlessStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
